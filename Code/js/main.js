147978db4dfb0a1838fdd17b64346ac1d99e0f50
Merfoo <fauzimerfoo@gmail.com>
2014-03-16 11:30:19 -0700

    Going into one of the inputs selects whole text, added "Red" "Blue" headers for each corresponding column, started basic js code

--- Code/js/main.js
+++ Code/js/main.js
@@ -1,21 +1,31 @@
 var keycodes = { zero: 48, nine: 57, tab: 9 };
+var maxStickButtons = 24;
+var pressedThreshold = 0.5;
+var bgWhite = "backgroundWhite";
+var bgRed = "backgroundRed";
+var bgBlue = "backgroundBlue";
+var bgBlack = "backgroundBlack";
+
+// DOM elements
 var $matchNumber;
-// Red
-var $redAlliance = [];
-var $redAutoData = [];
-var $redAutoZone;
-var $redTeleopData = [];
-var $redTeleopZone;
-var $redTags;
-var $redComments;
-// Blue
-var $blueAlliance = [];
-var $blueAutoData = [];
-var $blueAutoZone;
-var $blueTeleopData = [];
-var $blueTeleopZone;
-var $blueTags;
-var $blueComments;
+var $alliance = [[], []];
+var $autoData = [[], []];
+var $autoZone = [];
+var $teleopData = [[], []];
+var $teleopZone = [];
+var $tags = [];
+var $comments = [];
+
+// Data for each robot
+var autoData = [[], [], [], [], [], []];
+var autoZone = [bgBlack, bgBlack, bgBlack, bgBlack, bgBlack, bgBlack];
+var teleopData = [[], [], [], [], [], []];
+var teleopZone = [bgBlack, bgBlack, bgBlack, bgBlack, bgBlack, bgBlack];
+var tags = ["", "", "", "", "", ""];
+var comments = ["", "", "", "", "", ""]; 
+
+var joysticks = [0, 0];
+var teamIndexs = [0, 0];
 
 $(document).ready(init);
 
@@ -23,58 +33,103 @@ $(document).ready(init);
 function init()
 {
     $("input").keypress(textInputCallback);
+    $(document).on('click','input[type=text]',function(){ this.select(); });
     $matchNumber = $("#matchNumber")[0];
     $matchNumber.value = 0;
-    // Red
-    $redAlliance = $("*.team[id*=red]");
-    $redAutoData = $("input.data[id*=redAuto");
-    $redAutoZone = $("#redAutoZone")[0];
-    $redTeleopData = $("input.data[id*=redTeleop");
-    $redTeleopZone = $("#redTeleopZone")[0];
-    $redTags = $("#redTags")[0];
-    $redComments = $("#redComments")[0];
-    // Blue
-    $blueAlliance = $("*.team[id*=blue]");
-    $blueAutoData = $("input.data[id*=blueAuto");
-    $blueAutoZone = $("#blueAutoZone")[0];
-    $blueTeleopData = $("input.data[id*=blueTeleop");
-    $blueTeleopZone = $("#blueTeleopZone")[0];
-    $blueTags = $("#blueTags")[0];
-    $blueComments = $("#blueComments")[0];
-    print("Inited");
     
+    // Red
+    $alliance[0] = $("*.team[id*=red]");
+    $autoData[0] = $("input.data[id*=redAuto");
+    $autoZone[0] = $("#redAutoZone")[0];
+    $teleopData[0] = $("input.data[id*=redTeleop");
+    $teleopZone[0] = $("#redTeleopZone")[0];
+    $tags[0] = $("#redTags")[0];
+    $comments[0] = $("#redComments")[0];
+    
+    // Blue
+    $alliance[1] = $("*.team[id*=blue]");
+    $autoData[1] = $("input.data[id*=blueAuto");
+    $autoZone[1] = $("#blueAutoZone")[0];
+    $teleopData[1] = $("input.data[id*=blueTeleop");
+    $teleopZone[1] = $("#blueTeleopZone")[0];
+    $tags[1] = $("#blueTags")[0];
+    $comments[1] = $("#blueComments")[0];
+    
+    print("Inited");
     reset();
+    main();
 }
 
 // Resets everything in the scouting application
 function reset()
 {
     $matchNumber.value = 1 + ($matchNumber.value - 0);
-    for(var teamIndex = 0; teamIndex < $redAlliance.length; teamIndex++)
-        $redAlliance[teamIndex].value = $blueAlliance[teamIndex].value = "Team " + (teamIndex + 1);
     
-    for(var dataIndex = 0; dataIndex < $redAutoData.length; dataIndex++)
+    for(var stickIndex = 0; stickIndex < joysticks.length; stickIndex++)
     {
-        $redAutoData[dataIndex].value = $redTeleopData[dataIndex].value = 0;
-        $blueAutoData[dataIndex].value = $blueTeleopData[dataIndex].value = 0;
-    }
+        joysticks[stickIndex] = new Joystick();
+        teamIndexs[stickIndex] = 0;
+        
+        for(var teamIndex = 0; teamIndex < $alliance[0].length; teamIndex++)
+        {
+            $alliance[stickIndex][teamIndex].value = "Team " + (teamIndex + 1);
+            autoData[teamIndex] = 0;
+            autoZone[teamIndex] = bgBlack;
+            teleopData[teamIndex] = 0;
+            teleopZone[teamIndex] = bgBlack;
+            tags[teamIndex] = "";
+            comments[teamIndex] = ""; 
+        }
     
-    for(var dataIndex = 0; dataIndex < $redTags.length; dataIndex++)
+        for(var dataIndex = 0; dataIndex < $autoData[0].length; dataIndex++)
+            $autoData[stickIndex][dataIndex].value = $teleopData[stickIndex][dataIndex].value = 0;
+
+        for(var dataIndex = 0; dataIndex < $tags.length; dataIndex++)
+            $tags[stickIndex].value = $comments[stickIndex].value = "";
+
+        $autoZone[stickIndex].classList.remove(bgWhite, bgRed, bgBlue);
+        $autoZone[stickIndex].classList.add(bgBlack);
+        $teleopZone[stickIndex].classList.remove(bgWhite, bgRed, bgBlue);
+        $teleopZone[stickIndex].classList.add(bgBlack);
+    }
+}
+
+// Main loop for program
+function main()
+{
+    updateJoysticks();
+    window.webkitRequestAnimationFrame(main);
+}
+
+// Updates joystick
+function updateJoysticks()
+{
+    var rawSticks = navigator.webkitGetGamepads();
+    
+    for(var stickIndex = 0; stickIndex < rawSticks.length && stickIndex < 2; stickIndex++)
     {
-        $redTags[dataIndex].value = $redComments[dataIndex].value = "";
-        $blueTags[dataIndex].value = $blueComments[dataIndex].value = "";
+        var tmpStick = rawSticks[stickIndex];
+        
+        if(tmpStick)
+        {
+            // All non-joystick stick buttons
+            for(var buttonIndex = 0; buttonIndex < tmpStick.buttons.length && buttonIndex < maxStickButtons; buttonIndex++)
+                joysticks[stickIndex].updateButton(buttonIndex, tmpStick.buttons[buttonIndex] > pressedThreshold);
+
+            /* Subtracting 1 from each button value because array index are 0-based */
+        //    // Left stick
+        //    joysticks[stickIndex].updateButton(17 - 1, tmpStick.axes[0] > pressedThreshold);   // Button 17
+        //    joysticks[stickIndex].updateButton(19 - 1, tmpStick.axes[0] < -pressedThreshold);  // Button 19
+        //    joysticks[stickIndex].updateButton(18 - 1, tmpStick.axes[1] > pressedThreshold);   // Button 18
+        //    joysticks[stickIndex].updateButton(20 - 1, tmpStick.axes[1] < -pressedThreshold);  // Button 20
+        //
+        //    // Right stick
+        //    joysticks[stickIndex].updateButton(21 - 1, tmpStick.axes[2] > pressedThreshold);   // Button 21
+        //    joysticks[stickIndex].updateButton(23 - 1, tmpStick.axes[2] < -pressedThreshold);  // Button 23
+        //    joysticks[stickIndex].updateButton(22 - 1, tmpStick.axes[3] > pressedThreshold);   // Button 22
+        //    joysticks[stickIndex].updateButton(24 - 1, tmpStick.axes[3] < -pressedThreshold);  // Button 24
+        }    
     }
-    
-    // Red
-    $redAutoZone.classList.remove("backgroundWhite", "backgroundRed", "backgroundBlue");
-    $redAutoZone.classList.add("backgroundBlack");
-    $redTeleopZone.classList.remove("backgroundWhite", "backgroundRed", "backgroundBlue");
-    $redTeleopZone.classList.add("backgroundBlack");
-    // Blue
-    $blueAutoZone.classList.remove("backgroundWhite", "backgroundRed", "backgroundBlue");
-    $blueAutoZone.classList.add("backgroundBlack");
-    $blueTeleopZone.classList.remove("backgroundWhite", "backgroundRed", "backgroundBlue");
-    $blueTeleopZone.classList.add("backgroundBlack");
 }
 
 // Prevents input from entering a non-number input
@@ -91,3 +146,29 @@ function print(str)
 {
     console.log(str);
 }
+
+// CLASSES
+function Joystick()
+{
+    this.buttons = [maxStickButtons];
+    this.rawButtons = [maxStickButtons];
+    
+    for(var i = 0; i < maxStickButtons; i++)
+        this.buttons[i] = this.rawButtons[i] = false;
+}
+
+Joystick.prototype.getButton = function(index)
+{
+    return this.buttons[index];
+};
+
+Joystick.prototype.getRawButton = function(index)
+{
+    return this.rawButtons[index];
+};
+
+Joystick.prototype.updateButton = function(index, rawVal)
+{
+    this.buttons[index] = this.rawButtons[index] && !rawVal;
+    this.rawButtons[index] = rawVal;
+};
